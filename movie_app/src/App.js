6b3f53d83308cc0460920e6ed1118b08fdf147b4
박정호 <qkrwjdgh0904@naver.com>
2020-05-07 18:02:11 +0900

    [수정] 함수 호출로 props 전달

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,29 +1,35 @@
 import React from 'react';
 
-function Food({name}){
-  return <h1>I like {name}</h1>
+function Food({name, age}){
+  return <h1>I like {name}. I'm {age} years old.</h1>;
 }
 
 const list = [
   {
+    id:1,
     name: "가가가",
     age: 20
   },
   {
+    id:2,
     name: "나나나",
     age: 25
   },
   {
+    id:3,
     name: "다다다",
     age: 30
   },
 ];
 
+function renderList(man){
+  return <Food key={man.id} name={man.name} age={man.age}/>;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello~!!</h1>
-      {list.map(now => <Food name={now.name}/>)}
+      {list.map(renderList)}
     </div>
   );
 }
