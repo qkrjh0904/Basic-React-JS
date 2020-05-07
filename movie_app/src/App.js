2e535760a0550ad2cf564bf78f3d4f360c7e8ca5
박정호 <qkrwjdgh0904@naver.com>
2020-05-07 17:51:23 +0900

    [추가] 동적으로 props 넘기기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+function Food({name}){
+  return <h1>I like {name}</h1>
+}
+
+const list = [
+  {
+    name: "가가가",
+    age: 20
+  },
+  {
+    name: "나나나",
+    age: 25
+  },
+  {
+    name: "다다다",
+    age: 30
+  },
+];
+
 function App() {
   return (
     <div>
       <h1>Hello~!!</h1>
+      {list.map(now => <Food name={now.name}/>)}
     </div>
   );
 }
