29fe19b9002aa191301d49887ddef8fd01cf5cd8
박정호 <qkrwjdgh0904@naver.com>
2020-05-11 18:00:08 +0900

    [추가] CSS 적용

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -17,7 +17,7 @@ function Movie({ year, title, summary, poster, genres }) {
                         </li>
                     ))}
                 </ul>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 140)}...</p>
             </div>
         </div>
     );
