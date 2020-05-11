8fafa9eb26940ec4fe84af4347c4f39c6d1d9b23
박정호 <qkrwjdgh0904@naver.com>
2020-05-11 17:13:41 +0900

    [추가] API로 영화 데이터 fetch하기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
+import Movie from './Movie';
 // import PropTypes from 'prop-types';
 
 class App extends React.Component {
@@ -8,15 +9,38 @@ class App extends React.Component {
         movies: [],
     };
     getMovies = async () => {
-        const movies = await Axios.get('https://yts.mx/api/v2/list_movies.json');
+        await Axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+            .then((res) => {
+                const movies = res.data.data.movies;
+                // console.log(movies);
+                this.setState({ movies, isLoading: false });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
     componentDidMount() {
         this.getMovies();
     }
     render() {
-        const { isLoading } = this.state;
-        console.log('render');
-        return <div>{isLoading ? 'Loading' : 'We are ready'}</div>;
+        const { isLoading, movies } = this.state;
+        // console.log('render');
+        return (
+            <div>
+                {isLoading
+                    ? 'Loading'
+                    : movies.map((movie) => (
+                          <Movie
+                              key={movie.id}
+                              id={movie.id}
+                              year={movie.year}
+                              title={movie.title}
+                              summary={movie.summary}
+                              poster={movie.medium_cover_image}
+                          />
+                      ))}
+            </div>
+        );
     }
 }
 
